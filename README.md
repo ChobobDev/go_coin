a8675bcacb6ac0c8fb548e04de29a031e1e35580
chobobdev <chobobdev@naver.com>
2021-06-15 23:46:06 +0900

    #3.1 Variables in Go - 15/06/2021

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # go_coin
 
 15/06/2021 #3.0 Creating the Project 
+15/06/2021 #3.1 Variables in Go
--- main.go
+++ main.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+const name string = "Bernie"
+const age int = 21
+
 func main() {
+	var fr_name string = "Jeong"
+	var species string = "gugu"
 	fmt.Println("Blockchain study with Go lang")
+	fmt.Printf("name: %s age:%d \n", name, age)
+	fmt.Printf("이름 : %s 종: %s \n", fr_name, species)
 }
