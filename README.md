d9472c8e328bbbf23c3caac93eff29d8d229d767
chobobdev <chobobdev@naver.com>
2021-06-16 22:55:19 +0900

    #3.2 Functions -16/06/2021

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # go_coin
 
-15/06/2021 #3.0 Creating the Project 
-15/06/2021 #3.1 Variables in Go
+
+|             Date              |  Section  |
+| :---------------------------: |:------:|
+|15/06/2021| #3.0 Creating the Project |
+|15/06/2021| #3.1 Variables in Go|
+|16/06/2021| #3.2 Functions|
--- main.go
+++ main.go
@@ -2,13 +2,34 @@ package main
 
 import "fmt"
 
-const name string = "Bernie"
-const age int = 21
+// const name string = "Bernie"
+// const age int = 21
+
+func addtion(a int, b int) int {
+	return a + b
+}
+
+func something(number int, name string) (int, string) {
+	return number, name
+}
+
+func popo(numbers ...int) int {
+	var total int
+	for _, item := range numbers {
+		total += item
+	}
+	return total
+}
 
 func main() {
-	var fr_name string = "Jeong"
-	var species string = "gugu"
+	// var fr_name string = "Jeong"
+	// var species string = "gugu"
 	fmt.Println("Blockchain study with Go lang")
-	fmt.Printf("name: %s age:%d \n", name, age)
-	fmt.Printf("이름 : %s 종: %s \n", fr_name, species)
+	// fmt.Printf("name: %s age:%d \n", name, age)
+	// fmt.Printf("이름 : %s 종: %s \n", fr_name, species)
+	fmt.Println(addtion(1, 2))
+	fmt.Println(something(1, "bernie"))
+	sums := popo(2, 3, 4, 5, 6, 7)
+
+	fmt.Println(sums)
 }
