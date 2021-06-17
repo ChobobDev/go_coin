9198866f4344bf6459288d2cfaf3e4e9cd27213b
chobobdev <chobobdev@naver.com>
2021-06-17 22:32:47 +0900

    #3.3 fmt - 17/06/2021

--- main.go
+++ main.go
@@ -2,34 +2,20 @@ package main
 
 import "fmt"
 
-// const name string = "Bernie"
-// const age int = 21
-
-func addtion(a int, b int) int {
-	return a + b
-}
-
-func something(number int, name string) (int, string) {
-	return number, name
-}
-
-func popo(numbers ...int) int {
-	var total int
-	for _, item := range numbers {
-		total += item
-	}
-	return total
-}
-
 func main() {
-	// var fr_name string = "Jeong"
-	// var species string = "gugu"
-	fmt.Println("Blockchain study with Go lang")
-	// fmt.Printf("name: %s age:%d \n", name, age)
-	// fmt.Printf("이름 : %s 종: %s \n", fr_name, species)
-	fmt.Println(addtion(1, 2))
-	fmt.Println(something(1, "bernie"))
-	sums := popo(2, 3, 4, 5, 6, 7)
+	x := 16
+	fmt.Printf("%b \n", x)
+	fmt.Printf("%o \n", x)
+	fmt.Printf("%x \n", x)
+	fmt.Printf("%U \n", x)
 
-	fmt.Println(sums)
+	xAsBinary := fmt.Sprintf("%b", x)
+	xAsOct := fmt.Sprintf("%o", x)
+	xAsHex := fmt.Sprintf("%x", x)
+	xAsUnicode := fmt.Sprintf("%U", x)
+
+	fmt.Println(x, xAsBinary)
+	fmt.Println(x, xAsOct)
+	fmt.Println(x, xAsHex)
+	fmt.Println(x, xAsUnicode)
 }
