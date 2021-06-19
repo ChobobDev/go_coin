4199bca498acd7a36bbb681905e4e672c661e5da
chobobdev <chobobdev@naver.com>
2021-06-19 22:37:59 +0900

    #3.6 Structs - 19/06/2021

--- main.go
+++ main.go
@@ -2,9 +2,18 @@ package main
 
 import "fmt"
 
+type person struct {
+	name        string
+	age         int
+	nationality string
+}
+
+func (p person) greeting() {
+	fmt.Printf("Hello! My name is %s and I'm %d yers old, and I'm from %s", p.name, p.age, p.nationality)
+
+}
+
 func main() {
-	a := 2
-	b := &a
-	a = 10
-	fmt.Println(*b, a)
+	bernie := person{"Bernie", 21, "Korea"}
+	bernie.greeting()
 }
