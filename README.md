321c88831e5bdef8bd0c6ccd082b2d64122ddc7d
chobobdev <chobobdev@naver.com>
2021-06-27 10:11:53 +0900

    #6.4 NewServeMux - 27/06/2021

--- README.md
+++ README.md
@@ -45,4 +45,5 @@
 |26/06/2021|#6.1 Marshal and Field Tags|
 |26/06/2021|#6.2 MarshalText|
 |27/06/2021|#6.3 JSON Decode|
+|27/06/2021|#6.4 NewServeMux|
 
--- explorer/explorer.go
+++ explorer/explorer.go
@@ -9,7 +9,6 @@ import (
 )
 
 const (
-	port        string = ":4000"
 	templateDir string = "explorer/templates/"
 )
 
@@ -38,11 +37,12 @@ func add(rw http.ResponseWriter, r *http.Request) {
 
 }
 
-func Start() {
+func Start(port int) {
+	handler := http.NewServeMux()
 	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
 	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
-	http.HandleFunc("/", home)
-	http.HandleFunc("/add", add)
-	fmt.Printf("Litening on http://localhost%s\n", port)
-	http.ListenAndServe(port, nil)
+	handler.HandleFunc("/", home)
+	handler.HandleFunc("/add", add)
+	fmt.Printf("Litening on http://localhost:%d\n", port)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), handler)
 }
