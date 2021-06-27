e04bc3c12fd558de0a5e64a9f2db5a91f1998b3f
chobobdev <chobobdev@naver.com>
2021-06-27 09:42:59 +0900

    #6.3 JSON Decode - 27/06/2021

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/ChobobDev/go_coin/blockchain"
+	"github.com/ChobobDev/go_coin/utils"
 )
 
 const port string = ":4000"
@@ -22,6 +25,10 @@ type URLDescription struct {
 	Method      string `json:"method"`
 	Description string `json:"description"`
 	Payload     string `json:"payload,omitempty"`
+}
+
+type AddBlockBody struct {
+	Message string
 }
 
 func documentation(rw http.ResponseWriter, r *http.Request) {
@@ -43,8 +50,24 @@ func documentation(rw http.ResponseWriter, r *http.Request) {
 
 }
 
+func blocks(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		rw.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(rw).Encode(blockchain.GetBlockchain().AllBlocks())
+
+	case "POST":
+		var addBlockBody AddBlockBody
+		utils.HandleErr(json.NewDecoder(r.Body).Decode(&addBlockBody))
+		blockchain.GetBlockchain().AddBlock(addBlockBody.Message)
+		rw.WriteHeader(http.StatusCreated)
+
+	}
+}
+
 func main() {
 	http.HandleFunc("/", documentation)
+	http.HandleFunc("/blocks", blocks)
 	fmt.Printf("Litening on http://localhost%s\n", port)
 	log.Fatal((http.ListenAndServe(port, nil)))
 
