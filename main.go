d297b65b688e4702f83478f1da14c36609bd35e5
chobobdev <chobobdev@naver.com>
2021-07-12 23:34:28 +0900

    #8.5 Restoring the Blockchain - 12/07/2021

--- main.go
+++ main.go
@@ -3,7 +3,5 @@ package main
 import "github.com/ChobobDev/go_coin/blockchain"
 
 func main() {
-	blockchain.Blockchain().AddBlock("First")
-	blockchain.Blockchain().AddBlock("Second")
-	blockchain.Blockchain().AddBlock("Third")
+	blockchain.Blockchain()
 }
