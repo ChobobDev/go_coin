b87db4d0e56d1b0f4b520175d26f0c7fd8093d90
chobobdev <chobobdev@naver.com>
2021-07-18 22:31:53 +0900

    9.2 Mine Block - 18/07/2021

--- main.go
+++ main.go
@@ -1,22 +1,5 @@
 package main
 
-import (
-	"crypto/sha256"
-	"fmt"
-	"strings"
-)
-
 func main() {
-	difficulty := 2
-	target := strings.Repeat("0", difficulty)
-	nonce := 1
-	for {
-		hash := fmt.Sprintf("%x\n", sha256.Sum256([]byte("hello"+fmt.Sprint(nonce))))
-		fmt.Printf("Hash : %s\nTarget : %s\nNonce : %d", hash, target, nonce)
-		if strings.HasPrefix(hash, target) {
-			return
-		} else {
-			nonce++
-		}
-	}
+	//nothing for now
 }
